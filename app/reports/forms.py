154e4355936fc981d8f8a1b9b24f73e583bc7bda
Robert Hallam <roboffice100@gmail.com>
2019-01-23 15:44:30 +0000

    Filter by risk score functionality added and search form route structures fixed

--- app/reports/forms.py
+++ app/reports/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SelectField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, NumberRange
 
 class SearchStaffForm(FlaskForm):
     staff_name = StringField('Staff Name', validators=[DataRequired()])
@@ -9,3 +9,7 @@ class SearchStaffForm(FlaskForm):
 class SearchDepartmentForm(FlaskForm):
     department = SelectField('Department', coerce=int, validators=[DataRequired()])
     submit = SubmitField('Search Department')
+
+class SearchRiskScoreForm(FlaskForm):
+    risk_score = IntegerField('Filter by Risk Score', validators=[DataRequired('Input must be a positive integer.'), NumberRange(0, 100, 'Risk score must be between 0 and 100.')])
+    submit = SubmitField('Search Staff')
--- app/reports/routes.py
+++ app/reports/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, flash, request, current_app, session
 from flask_login import login_required
 from app.reports import bp
-from app.reports.forms import SearchStaffForm, SearchDepartmentForm
+from app.reports.forms import SearchStaffForm, SearchDepartmentForm, SearchRiskScoreForm
 from app.models import Staff, Departments
 from sqlalchemy import func
 
@@ -10,27 +10,39 @@ def before_request():
     session.permanent = True # lifetime length in config
 
 
-@bp.route('/reports', methods=['GET'])
+@bp.route('/reports', methods=['GET', 'POST'])
 @login_required
 def reports():
-    page = request.args.get('page', 1, type=int)
-    staff = Staff.query.order_by(Staff.risk_score.desc()).paginate(page, current_app.config['POSTS_PER_PAGE'], False)
-    if staff.has_next:
-        next_url = url_for('reports.reports', page=staff.next_num)
+    form = SearchRiskScoreForm()
+    if form.validate_on_submit():
+        page = request.args.get('page', 1, type=int)
+        staff = Staff.query.filter(Staff.risk_score>=form.risk_score.data).order_by(Staff.risk_score.desc()).paginate(page, current_app.config['POSTS_PER_PAGE'], False)
+        if staff.has_next:
+            next_url = url_for('reports.reports', page=staff.next_num)
+        else:
+            next_url = None
+        if staff.has_prev:
+            prev_url = url_for('reports.reports', page=staff.prev_num)
+        else:
+            prev_url = None
+        return render_template('reports/reports.html', title='Reports', form=form, staff=staff.items, next_url=next_url, prev_url=prev_url)
     else:
-        next_url = None
-    if staff.has_prev:
-        prev_url = url_for('reports.reports', page=staff.prev_num)
-    else:
-        prev_url = None
-    return render_template('reports/reports.html', title='Reports', staff=staff.items, next_url=next_url, prev_url=prev_url)
+        page = request.args.get('page', 1, type=int)
+        staff = Staff.query.order_by(Staff.risk_score.desc()).paginate(page, current_app.config['POSTS_PER_PAGE'], False)
+        if staff.has_next:
+            next_url = url_for('reports.reports', page=staff.next_num)
+        else:
+            next_url = None
+        if staff.has_prev:
+            prev_url = url_for('reports.reports', page=staff.prev_num)
+        else:
+            prev_url = None
+        return render_template('reports/reports.html', title='Reports', form=form, staff=staff.items, next_url=next_url, prev_url=prev_url)
 
 @bp.route('/reports/staff', methods=['GET', 'POST'])
 @login_required
 def reports_staff():
     form = SearchStaffForm()
-    if request.method == "GET":
-        return render_template('reports/reports_staff.html', title='Search Staff', form=form)
     if form.validate_on_submit():
         page = request.args.get('page', 1, type=int)
         staff = Staff.query.filter(
@@ -45,14 +57,14 @@ def reports_staff():
         else:
             prev_url = None
         return render_template('reports/reports_staff.html', title='Search Staff', form=form, staff=staff.items, next_url=next_url, prev_url=prev_url)
+    else:
+        return render_template('reports/reports_staff.html', title='Search Staff', form=form)
 
 @bp.route('/reports/departments', methods=['GET', 'POST'])
 @login_required
 def reports_departments():
     form = SearchDepartmentForm()
     form.department.choices = [(i.id, i.department_name) for i in Departments.query.order_by('department_name')]
-    if request.method == "GET":
-        return render_template('reports/reports_departments.html', title='Search Department', form=form)
     if form.validate_on_submit():
         department = Departments.query.get(form.department.data)
         page = request.args.get('page', 1, type=int)
@@ -66,3 +78,35 @@ def reports_departments():
         else:
             prev_url = None
         return render_template('reports/reports_departments.html', title='Search Department', form=form, department=department, staff=staff.items, next_url=next_url, prev_url=prev_url)
+    else:
+        return render_template('reports/reports_departments.html', title='Search Department', form=form)
+
+    # POST/REDIRECT/GET pattern (shown below) does not work! Redirect will result in paginated object being re-generated, making form useless.
+
+    # form = SearchDepartmentForm()
+    # form.department.choices = [(i.id, i.department_name) for i in Departments.query.order_by('department_name')]
+    # page = request.args.get('page', 1, type=int)
+    # staff = Staff.query.order_by(Staff.staff_name).paginate(page, current_app.config['POSTS_PER_PAGE'], False)
+    # department = None
+    # if staff.has_next:
+    #     next_url = url_for('reports.reports_departments', page=staff.next_num)
+    # else:
+    #     next_url = None
+    # if staff.has_prev:
+    #     prev_url = url_for('reports.reports_departments', page=staff.prev_num)
+    # else:
+    #     prev_url = None
+    # if form.validate_on_submit():
+    #     department = Departments.query.get(form.department.data)
+    #     page = request.args.get('page', 1, type=int)
+    #     staff = Staff.query.filter(Staff.department_id==form.department.data).order_by(Staff.risk_score.desc()).paginate(page, current_app.config['POSTS_PER_PAGE'], False)
+    #     if staff.has_next:
+    #         next_url = url_for('reports.reports_departments', page=staff.next_num)
+    #     else:
+    #         next_url = None
+    #     if staff.has_prev:
+    #         prev_url = url_for('reports.reports_departments', page=staff.prev_num)
+    #     else:
+    #         prev_url = None
+    #     return redirect(url_for('reports.reports_departments'))
+    # return render_template('reports/reports_departments.html', title='Search Department', form=form, department=department, staff=staff.items, next_url=next_url, prev_url=prev_url)
